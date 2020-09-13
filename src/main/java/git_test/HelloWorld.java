045a5c2b8c78c9c429bd3368982e6cfdbfd5ecab
qujianhua582 <71180824+qujianhua582@users.noreply.github.com>
2020-09-13 16:43:43 +0800

    Update HelloWorld.java

--- src/main/java/git_test/HelloWorld.java
+++ src/main/java/git_test/HelloWorld.java
@@ -3,5 +3,6 @@ package git_test;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("HelloWorld!");
+        System.out.println("HelloWorld!");
     }
 }
